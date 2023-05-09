2c42c9653ca295d1657e2ff19fb81512a3a67d9d
Anudeep Reddy <95900205+Crazydeepu@users.noreply.github.com>
2023-05-09 20:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,4 +10,8 @@ var team = players;
 var team1 = players;
 var cap1 = person;
 
-
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
